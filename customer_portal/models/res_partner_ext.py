7ee892f20bfbec15d3269ae0a6cf7d5b0daad1f1
Faizan <faizan.ali@axiomworld.net>
2025-05-28 21:17:35 +0500

    [Test] Test

--- customer_portal/models/res_partner_ext.py
+++ customer_portal/models/res_partner_ext.py
@@ -4,7 +4,5 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-
-
     username = fields.Char(string='Username', required=True, unique=True)
     password = fields.Char(string='Password', required=True)
