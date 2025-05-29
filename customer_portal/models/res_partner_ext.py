9bce49f3e1b39eda9e6cb88c138c717269f7571a
Faizan <faizan.ali@axiomworld.net>
2025-05-29 10:31:03 +0500

    [Test] Test

--- customer_portal/models/res_partner_ext.py
+++ customer_portal/models/res_partner_ext.py
@@ -5,5 +5,10 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
     
     
+    
+    
+    
+    
+    
     username = fields.Char(string='Username', required=True, unique=True)
     password = fields.Char(string='Password', required=True)
